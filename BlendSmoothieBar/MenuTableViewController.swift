2bcbd5a9357f58f4f8112a170c1851d4f1d0cb2a
technology08 <cjespenshade@yahoo.com>
2017-10-09 22:48:10 -0400

    Deleted old UI and fixed UINavigationItem title.

--- BlendSmoothieBar/MenuTableViewController.swift
+++ BlendSmoothieBar/MenuTableViewController.swift
@@ -16,6 +16,8 @@ class MenuTableViewController: UITableViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
+        self.navigationItem.title = "Menu"
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
