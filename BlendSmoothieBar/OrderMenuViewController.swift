1357001196e001f666ca4a7279d7f1267ddfaf02
technology08 <cjespenshade@yahoo.com>
2017-10-13 13:36:50 -0400

    Added Stripe framework.

--- BlendSmoothieBar/OrderMenuViewController.swift
+++ BlendSmoothieBar/OrderMenuViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import PassKit
+import Stripe
 
-class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ModifierSwitchDelegate {
+class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ModifierSwitchDelegate, PKPaymentAuthorizationViewControllerDelegate {
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
     
     @IBOutlet weak var productTypeLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
@@ -16,6 +21,7 @@ class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableVie
     
     @IBOutlet weak var tableViewHeightConstraint: NSLayoutConstraint!
     
+    var applePayButton: UIButton?
     var currentProductCategory: ProductTypes = .Smoothie
     var selectedProduct: Product? {
         didSet {
@@ -23,12 +29,21 @@ class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableVie
         }
     }
     
+    @IBOutlet var addMoreItemsButton: UIButton!
+    @IBOutlet var cashButton: UIButton!
+    @IBOutlet var cashButtonTrailingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var cashButtonLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var cashButtonBottomConstraint: NSLayoutConstraint!
+    
+    var supportedNetworks: [PKPaymentNetwork] = [.visa, .masterCard, .amex, .discover /*, .JCB*/]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         
         parameterTableView.delegate = self
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -40,6 +55,35 @@ class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableVie
         super.updateViewConstraints()
         
         self.tableViewHeightConstraint.constant = self.parameterTableView.contentSize.height
+        
+        if PKPaymentAuthorizationViewController.canMakePayments() {
+            if #available(iOS 10.0, *) {
+                applePayButton = PKPaymentButton(paymentButtonType: .inStore, paymentButtonStyle: .white)
+            } else {
+                // Fallback on earlier versions
+                applePayButton = PKPaymentButton(paymentButtonType: .buy, paymentButtonStyle: .white)
+            }
+            applePayButton?.addTarget(self, action: #selector(applePayButtonPressed), for: .touchUpInside)
+            applePayButton?.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(applePayButton!)
+            
+            cashButtonTrailingConstraint.isActive = false
+            cashButtonLeadingConstraint.constant = 8
+            cashButtonBottomConstraint.constant = 8
+            cashButton.layer.cornerRadius = 5
+
+            let leadingConstraint = applePayButton?.leadingAnchor.constraint(equalTo: cashButton.trailingAnchor, constant: 8)
+            let trailingConstraint = applePayButton?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -8)
+            let bottomConstraint = applePayButton?.bottomAnchor.constraint(equalTo: addMoreItemsButton.topAnchor, constant: -8)
+            let equalWidths = applePayButton?.widthAnchor.constraint(equalTo: cashButton.widthAnchor)
+            let height = applePayButton?.heightAnchor.constraint(equalToConstant: 44)
+            
+            NSLayoutConstraint.activate([leadingConstraint!, trailingConstraint!, bottomConstraint!, equalWidths!, height!])
+            
+        } else {
+            //Extend cash button full width
+            
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,16 +93,19 @@ class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableVie
         case ProductTypes.Smoothie:
             productTypeLabel.text = "Smoothie"
             selectedProduct = currentSmoothies.first
+            visualEffectView.effect = UIBlurEffect(style: .dark)
+            backgroundImageView.image = #imageLiteral(resourceName: "smoothie")
         case ProductTypes.Shake:
             productTypeLabel.text = "Shake"
             selectedProduct = currentShakes.first
+            visualEffectView.effect = UIBlurEffect(style: .dark)
+            backgroundImageView.image = #imageLiteral(resourceName: "shake")
         case ProductTypes.Food:
             productTypeLabel.text = "Breakfast"
             selectedProduct = currentFoods.first
+            visualEffectView.effect = UIBlurEffect(style: .dark)
+            backgroundImageView.image = #imageLiteral(resourceName: "waffle")
         }
-    }
-    
-    func modifierValueDidChange(modifier: String, price: NSDecimalNumber, value: Bool) {
         
     }
     
@@ -147,6 +194,50 @@ class OrderMenuViewController: UIViewController, UITableViewDelegate, UITableVie
                 return tableView.dequeueReusableCell(withIdentifier: "parameterCell")!
             }
         }
+    }
+    
+    func modifierValueDidChange(modifier: String, price: NSDecimalNumber, value: Bool) {
+        
+    }
+    
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        
+    }
+    
+    @available(iOS 9.0, *)
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, completion: @escaping (PKPaymentAuthorizationStatus) -> Void) {
+        
+    }
+    
+    @available(iOS 11.0, *)
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        
+    }
+    
+    @objc func applePayButtonPressed() {
+        if PKPaymentAuthorizationViewController.canMakePayments(usingNetworks: supportedNetworks) {
+            let request = PKPaymentRequest()
+            request.merchantIdentifier = "INSERT_APPLE_MERCHANT_ID"
+            request.countryCode = "US"
+            request.currencyCode = "USD"
+            request.supportedNetworks = supportedNetworks
+            request.merchantCapabilities = .capability3DS //CHECK WITH STRIPE
+            
+            let item = PKPaymentSummaryItem(label: "\(selectedProduct?.name!) \(selectedProduct?.type)", amount: (selectedProduct?.price)!)
+            request.paymentSummaryItems = [item]
+            
+            let vc = PKPaymentAuthorizationViewController(paymentRequest: request)
+            vc?.delegate = self
+            present(vc!, animated: true, completion: nil)
+        } else {
+            //CHECK COPYRIGHT
+            let alert = UIAlertController(title: "Apple Pay Error", message: "No supported cards. Only Visa, Mastercard, American Express, and Discover cards are valid.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: { (action) in
+                alert.dismiss(animated: true, completion: nil)
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        
     }
     
     /*
