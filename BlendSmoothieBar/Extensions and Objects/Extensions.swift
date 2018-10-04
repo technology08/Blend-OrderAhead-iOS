cac5d818594bdd640a1574dfdf838eb594fb180c
technology08 <cjespenshade@yahoo.com>
2018-10-03 22:31:43 -0400

    Build 2, fixed simplified operating hours.

--- BlendSmoothieBar/Extensions and Objects/Extensions.swift
+++ BlendSmoothieBar/Extensions and Objects/Extensions.swift
@@ -70,6 +70,4 @@ public extension Int {
     }
 }
 
-let times = ["7:30 AM", "7:35 AM", "7:40 AM", "7:45 AM", "7:50 AM", "7:55 AM", "8:00 AM", "8:05 AM", "8:10 AM", "8:15 AM", "8:20 AM", "8:25 AM", "8:30 AM", "8:35 AM", "8:40 AM", "8:45 AM", "8:50 AM", "8:55 AM", "9:00 AM", "9:05 AM", "9:10 AM", "9:15 AM", "9:20 AM", "9:25 AM", "9:30 AM", "9:35 AM", "9:40 AM", "9:45 AM", "9:50 AM", "9:55 AM", "10:00 AM", "10:05 AM", "10:10 AM", "10:15 AM", "10:20 AM", "10:25 AM", "10:30 AM", "1:25 PM", "1:30 PM", "1:35 PM", "1:40 PM", "1:45 PM", "1:50 PM", "1:55 PM", "2:00 PM", "2:05 PM", "2:10 PM", "2:15 PM", "2:20 PM", "2:25 PM", "2:30 PM", "2:35 PM", "2:40 PM", "2:45 PM", "2:50 PM", "2:55 PM", "3:00 PM", "3:05 PM", "3:10 PM", "3:15 PM", "3:20 PM", "3:25 PM", "3:30 PM"]
-
-let dates = [Date.init(hour: 7, minute: 30), Date.init(hour: 7, minute: 35), Date.init(hour: 7, minute: 45), Date.init(hour: 7, minute: 50), Date.init(hour: 7, minute: 55), Date.init(hour: 8, minute: 00), Date.init(hour: 8, minute: 05), Date.init(hour: 8, minute: 10)]
+let times = ["2:30 PM", "2:35 PM", "2:40 PM", "2:45 PM", "2:50 PM", "2:55 PM", "3:00 PM", "3:05 PM", "3:10 PM", "3:15 PM", "3:20 PM", "3:25 PM", "3:30 PM", "3:35 PM", "3:40 PM", "3:45 PM"]
